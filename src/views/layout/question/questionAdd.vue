5bd9ea2e689c0c5e901d32aeaf919e9d0c52afe0
zzm <1054965228@qq.com>
2020-06-30 20:44:42 +0800

    文件格式化

--- src/views/layout/question/questionAdd.vue
+++ src/views/layout/question/questionAdd.vue
@@ -38,7 +38,11 @@
         </el-form-item>
         <!-- 城市选择 -->
         <el-form-item prop="city" label="城市">
-          <el-cascader v-model="form.city" :options="regionData" :props="{ value: 'label' }"></el-cascader>
+          <el-cascader
+            v-model="form.city"
+            :options="regionData"
+            :props="{ value: 'label' }"
+          ></el-cascader>
         </el-form-item>
         <!-- 题型 -->
         <el-form-item prop="type" label="题型">
@@ -47,7 +51,8 @@
             :label="+key"
             v-for="(value, key, index) in typeObj"
             :key="index"
-          >{{ value }}</el-radio>
+            >{{ value }}</el-radio
+          >
         </el-form-item>
         <!-- 难度 -->
         <el-form-item prop="difficulty" label="难度">
@@ -56,7 +61,8 @@
             :label="+key"
             v-for="(value, key, index) in diffObj"
             :key="index"
-          >{{ value }}</el-radio>
+            >{{ value }}</el-radio
+          >
         </el-form-item>
         <hr class="hr" />
         <!-- 试题标题 -->
@@ -69,7 +75,11 @@
         </el-form-item>
         <!-- 单选多选简答 -->
         <!-- 对象.变量命名，不能是数字中文，如果不是变量命名的规范要用中括号，对象[] -->
-        <el-form-item :label="typeObj[form.type]" :prop="typeAns[form.type]" label-width="60px">
+        <el-form-item
+          :label="typeObj[form.type]"
+          :prop="typeAns[form.type]"
+          label-width="60px"
+        >
           <!-- 把单选多选简答抽离出来 -->
           <selectItem :form="form" @validateItem="validateItem"></selectItem>
         </el-form-item>
@@ -98,90 +108,90 @@
 </template>
 
 <script>
-import { addQuestion, editQuestion } from "@/api/question";
-import upload from "@/components/upload";
-import { regionData } from "element-china-area-data";
-import { quillEditor } from "vue-quill-editor";
-import selectItem from "./selectItem";
-import "quill/dist/quill.core.css";
-import "quill/dist/quill.snow.css";
-import "quill/dist/quill.bubble.css";
+import { addQuestion, editQuestion } from '@/api/question';
+import upload from '@/components/upload';
+import { regionData } from 'element-china-area-data';
+import { quillEditor } from 'vue-quill-editor';
+import selectItem from './selectItem';
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.bubble.css';
 export default {
-  props: ["subjectList", "stepObj", "companyList", "typeObj", "diffObj"],
+  props: ['subjectList', 'stepObj', 'companyList', 'typeObj', 'diffObj'],
   components: {
     quillEditor, // 导入的富文本
     selectItem,
-    upload
+    upload,
   },
   data() {
     return {
-      initForm: "",
-      meta: "add",
+      initForm: '',
+      meta: 'add',
       typeAns: {
-        1: "single_select_answer",
-        2: "multiple_select_answer",
-        3: "short_answer"
+        1: 'single_select_answer',
+        2: 'multiple_select_answer',
+        3: 'short_answer',
       },
       regionData: regionData, // 导入的省市区，导入的数据不能直接在html里使用
       isShow: false,
       form: {
-        subject: "", // int	学科id标识
-        step: "", // int	阶段1、初级 2、中级 3、高级
-        enterprise: "", // int	企业id标识
+        subject: '', // int	学科id标识
+        step: '', // int	阶段1、初级 2、中级 3、高级
+        enterprise: '', // int	企业id标识
         city: [], // array	[省、市、区县]
         type: 1, // int	题型 1单选 、2多选 、3简答
         difficulty: 1, // int	题目难度 1简单 、2一般 、3困难
-        title: "", // string    标题
-        single_select_answer: "", // string	单选题答案
+        title: '', // string    标题
+        single_select_answer: '', // string	单选题答案
         multiple_select_answer: [], // array	多选题答案
-        short_answer: "", // string	简答题答案
-        video: "", // tring	解析视频地址
-        answer_analyze: "", // string	答案解析
-        remark: "", // string	答案备注
+        short_answer: '', // string	简答题答案
+        video: '', // tring	解析视频地址
+        answer_analyze: '', // string	答案解析
+        remark: '', // string	答案备注
         select_options: [
           {
-            label: "A",
-            text: "",
-            image: ""
+            label: 'A',
+            text: '',
+            image: '',
           },
           {
-            label: "B",
-            text: "",
-            image: ""
+            label: 'B',
+            text: '',
+            image: '',
           },
           {
-            label: "C",
-            text: "",
-            image: ""
+            label: 'C',
+            text: '',
+            image: '',
           },
           {
-            label: "D",
-            text: "",
-            image: ""
-          }
-        ] // array	选项，介绍，图片介绍
+            label: 'D',
+            text: '',
+            image: '',
+          },
+        ], // array	选项，介绍，图片介绍
       },
       rules: {
-        subject: [{ required: true, message: "必填", trigger: "change" }],
-        step: [{ required: true, message: "必填", trigger: "change" }],
-        enterprise: [{ required: true, message: "必填", trigger: "change" }],
-        city: [{ required: true, message: "必填", trigger: "change" }],
-        type: [{ required: true, message: "必填", trigger: "change" }],
-        difficulty: [{ required: true, message: "必填", trigger: "change" }],
-        title: [{ required: true, message: "必填", trigger: "change" }],
+        subject: [{ required: true, message: '必填', trigger: 'change' }],
+        step: [{ required: true, message: '必填', trigger: 'change' }],
+        enterprise: [{ required: true, message: '必填', trigger: 'change' }],
+        city: [{ required: true, message: '必填', trigger: 'change' }],
+        type: [{ required: true, message: '必填', trigger: 'change' }],
+        difficulty: [{ required: true, message: '必填', trigger: 'change' }],
+        title: [{ required: true, message: '必填', trigger: 'change' }],
         single_select_answer: [
-          { required: true, message: "必填", trigger: "change" }
+          { required: true, message: '必填', trigger: 'change' },
         ],
         multiple_select_answer: [
-          { required: true, message: "必填", trigger: "change" }
+          { required: true, message: '必填', trigger: 'change' },
         ],
-        short_answer: [{ required: true, message: "必填", trigger: "change" }],
-        video: [{ required: true, message: "必填", trigger: "change" }],
+        short_answer: [{ required: true, message: '必填', trigger: 'change' }],
+        video: [{ required: true, message: '必填', trigger: 'change' }],
         answer_analyze: [
-          { required: true, message: "必填", trigger: "change" }
+          { required: true, message: '必填', trigger: 'change' },
         ],
-        remark: [{ required: true, message: "必填", trigger: "change" }]
-      }
+        remark: [{ required: true, message: '必填', trigger: 'change' }],
+      },
     };
   },
   created() {
@@ -189,21 +199,21 @@ export default {
   },
   methods: {
     submit() {
-      this.$refs.form.validate(result => {
+      this.$refs.form.validate((result) => {
         if (!result) {
-          this.$message.warning("请填写相关内容");
+          this.$message.warning('请填写相关内容');
         } else {
-          if (this.meta == "add") {
+          if (this.meta == 'add') {
             addQuestion(this.form).then(() => {
-              this.$message.success("发布题目成功");
+              this.$message.success('发布题目成功');
               this.isShow = false;
-              this.$emit("search");
+              this.$emit('search');
             });
           } else {
             editQuestion(this.form).then(() => {
-              this.$message.success("编辑题目成功");
+              this.$message.success('编辑题目成功');
               this.isShow = false;
-              this.$emit("getData");
+              this.$emit('getData');
             });
           }
         }
@@ -211,7 +221,7 @@ export default {
     },
     validateItem(str) {
       this.$refs.form.validateField(str);
-    }
+    },
   },
   watch: {
     isShow(newVal) {
@@ -222,8 +232,8 @@ export default {
           this.$refs.form.clearValidate();
         });
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
